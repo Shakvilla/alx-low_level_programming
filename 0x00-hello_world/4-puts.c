0568b3e94c4935a93f70249514508294192ead52
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-09-08 16:40:50 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - Entry point to the application
+ *
+ * Return: Should be 0 to show success
+ *
+ * /
+ int main(void)
+ {
+	puts("Programming is like building a multilingual puzzle\n");
+	return(0);
+ }
