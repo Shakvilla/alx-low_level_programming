29a8fc6e2a7c6589d48f0eb6e42d0baf11597578
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-09-08 17:13:57 -0700

    refactored 4-puts.c to pass betty linter test

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry Point
  *
- * Return: Always 0 (Success)
- * 
+ * Return: Always 0
  */
 
  int main(void)
  {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts ("\"Programming is like building a multilingual puzzle");
+	return (0);
  }
