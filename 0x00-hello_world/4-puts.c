4b0701cd91bb162d78a67599ea87458aa842fc8c
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-09-08 17:05:32 -0700

    refactored 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - Entry point to the application
  *
- * Return: Should be 0 to show success
+ * main - Entry Point
  *
- * /
+ * Return: Always 0 (Success)
+ * 
+ */
+
  int main(void)
  {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
  }
