d3856c41f48d3134463a56284bad1dd7db63d5a1
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-09-09 16:06:53 -0700

    refacotored code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
- * main - Entry Point
+
+/**
+ * main - Checks to see if number is greater than zero, is equal to or negative.
  * 
  * Return: Always 0 (Success)
  *
@@ -10,6 +11,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
