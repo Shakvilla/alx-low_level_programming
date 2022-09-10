12b1ab5776066267f264daed69e525a5f726543f
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-09-09 17:55:00 -0700

    print alphabets in lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabets in lowercase and uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	char alphabets[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (a = 0; a < 52; a++)
+	{
+		putchar(alphabets([a]);
+	}
+	putchar('\n');
+	return (0);
+}
