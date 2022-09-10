7ffc82318da7f65c58cca8d724316172653f7274
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-09-09 19:04:40 -0700

    added print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digits of base 10
+ *
+ * Return: Return 0 if successful
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '10'; i++)
+	{
+		putchar(i)
+	}
+	putchar('\n');
+	return (0);
+}
