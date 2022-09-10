79d6ba2c9b83cd5f7da777855066d966da105605
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-09-09 17:40:03 -0700

    print alphabets  in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets is lowercase chars
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+
+	char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (int a = 0; a < 26; a++)
+	{
+		putchar(alphabet[a]);
+	}
+	putchar('\n');
+	return (0);
+}
