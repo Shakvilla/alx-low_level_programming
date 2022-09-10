38f2998a0b21f1cf4294cda020a88eb1d3748c47
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-09-09 18:45:37 -0700

    refactored code

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints all single digits of base 10
+ *
+ * Return: Return 0 if successful
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
