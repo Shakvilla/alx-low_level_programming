f77a604967d5af37b9b3cb21f4e72f7d0ae54007
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-09-13 15:27:53 -0700

    refactored code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,13 @@
 int main(void)
 {
 
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};                                                                                      
+
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+
 	int count, size;
+
 	size = sizeof(str) / sizeof(int);
+
 	for (count = 0; count < size; count++)
 	{
 		_putchar(str[count]);
