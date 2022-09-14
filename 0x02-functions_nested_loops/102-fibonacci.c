f465705e6a8d8fd11999c7f899843283e844de79
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-09-14 12:42:08 -0700

    refactored code

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Prints fibbonnacci series 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int c;
+	long int n1, n2, fn;
+
+	n1 = 1;
+	n2 = 2;
+
+	printf("%d, %d", n1, n2);
+
+	for (c = 0; c < 48; c++)
+	{
+		fn = n1 + n2;
+		printf(", %1d", fn);
+		n1 = n2;
+		n2 = fn;
+	}
+	printf("\n");
+	return (0);
+}
