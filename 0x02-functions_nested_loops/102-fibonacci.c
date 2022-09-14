debe6ccce70135013f00ec062fb6322887666a18
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-09-14 12:47:08 -0700

    refactored code

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,12 +13,12 @@ int main(void)
 	n1 = 1;
 	n2 = 2;
 
-	printf("%1d, %1d", n1, n2);
+	printf("%ld, %ld", n1, n2);
 
 	for (c = 0; c < 48; c++)
 	{
 		fn = n1 + n2;
-		printf(", %1d", fn);
+		printf(", %ld", fn);
 		n1 = n2;
 		n2 = fn;
 	}
