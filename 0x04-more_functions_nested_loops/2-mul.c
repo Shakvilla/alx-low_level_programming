346bb592e47e2f8277f22c24bf51db87b9033b6d
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-09-15 18:17:49 -0700

    added new file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *
+ * mul - multiplies two numbers
+ *
+ * @num1: first number
+ *
+ * @num2: second numbe
+ * 
+ * Return: Always (0)
+ *
+ */
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
