6428928ecd27b14df429a8439554c3300b5e07e6
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-10-25 13:11:13 -0700

    added new file

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+ * struct listint_s - singly liked list
+ * @n: integer
+ * @next: points to the next one
+ *
+ * Description: a singly linked list node structure
+ */
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+
+
+#endif
