9244f13120a5a89a4916b2d27ca26c157dc15063
Abdullah Clement Abdul Shakur <abdulshakuraclement@yahoo.com>
2022-10-31 18:15:54 -0700

    added new file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,11 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
+void print_addr(char *ptr);
+void print_type(char *ptr);
+void print_osabi(char *ptr);
+void print_version(char *ptr);
+void print_data(char *ptr);
+void print_magic(char *ptr);
+void check_sys(char *ptr);
 #endif
